c0616ddb2b1b23a1a1bb6be2fb858697fdb5ae1d
Githiber-mine <kartofanchik228fantik@gmail.com>
2025-07-26 00:03:04 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -218,7 +218,7 @@ async def main():
     await app.initialize()
     await app.start()
     await app.updater.start_polling()
-    await app.updater.idle()
+    await app.stop()
 
 
 # ✅ Точка входа
