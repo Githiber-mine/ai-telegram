e9f13a39f86c5e2156ec8a7854690c930884fdd9
Githiber-mine <kartofanchik228fantik@gmail.com>
2025-07-28 22:52:56 +0500

    Delete auth.py

--- auth.py
+++
@@ -1,9 +0,0 @@
-import json
-
-def load_admins(path="admins.json"):
-    try:
-        with open(path, "r", encoding="utf-8") as f:
-            data = json.load(f)
-            return set(data.get("admins", []))
-    except FileNotFoundError:
-        return set()
