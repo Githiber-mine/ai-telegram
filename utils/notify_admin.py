b2c0029ef2fe157ef3aefecf83b2bea1363cb06b
Githiber-mine <kartofanchik228fantik@gmail.com>
2025-07-28 23:27:47 +0500

    Update notify_admin.py

--- utils/notify_admin.py
+++ utils/notify_admin.py
@@ -1,1 +1,7 @@
+ADMIN_ID = 7029603268  # 🔁 Замени на свой Telegram user ID
 
+async def notify_admin(bot, text: str):
+    try:
+        await bot.send_message(chat_id=ADMIN_ID, text=text)
+    except Exception as e:
+        print(f"Ошибка отправки админу {ADMIN_ID}: {e}")
