04bac6bad8b094f0ad24fac711f7306957abca6f
Githiber-mine <kartofanchik228fantik@gmail.com>
2025-07-31 16:57:25 +0500

    Delete utils/cleaner.py

--- utils/cleaner.py
+++
@@ -1,8 +0,0 @@
-import re
-
-def clean_ai_reply(reply: str) -> str:
-    reply = re.sub(r"\*\*(.*?)\*\*", r"\1", reply)
-    reply = re.sub(r"__(.*?)__", r"\1", reply)
-    reply = re.sub(r"\*(.*?)\*", r"\1", reply)
-    reply = re.sub(r"_([^_]+)_", r"\1", reply)
-    return reply.strip()
